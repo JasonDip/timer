e084be799bb2c102e7236179efd271285bca24f9
Jason Dip <ContactJasonDip@gmail.com>
2020-05-15 09:17:58 -0700

    add wip layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,119 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-import { DatePicker } from "antd";
+import {
+    DatePicker,
+    Typography,
+    Divider,
+    Layout,
+    Space,
+    Card,
+    Menu,
+    Breadcrumb,
+    Row,
+    Col,
+    List,
+    Avatar,
+} from "antd";
 import "antd/dist/antd.css";
 
+import { MyComp } from "./components/mycomp";
+
 function App() {
+    const { Header, Sider, Content, Footer } = Layout;
+
+    const data = [
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+        {
+            title: "Test Title",
+        },
+    ];
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-            <DatePicker />
+            <Layout className="layout">
+                <Header>
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={["1"]}
+                    >
+                        <Menu.Item key="1">Home</Menu.Item>
+                        <Menu.Item key="2">Alarms</Menu.Item>
+                        <Menu.Item key="3">Settings</Menu.Item>
+                        <Menu.Item key="4">About</Menu.Item>
+                    </Menu>
+                </Header>
+                <Content className={"content"} style={{ padding: "25px" }}>
+                    <List
+                        style={{
+                            overflowY: "scroll",
+                            maxHeight: "100%",
+                        }}
+                        bordered
+                        itemLayout="horizontal"
+                        dataSource={data}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={
+                                        <Avatar src="https://img.favpng.com/17/16/9/alarm-clock-scalable-vector-graphics-icon-png-favpng-HGxKY3v7u1vtVJ3cS2HVb0yBc.jpg" />
+                                    }
+                                    title={
+                                        <a href="https://google.com">
+                                            {item.title}
+                                        </a>
+                                    }
+                                    description="test description"
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </Content>
+                <Footer style={{ textAlign: "center" }}>Jason Dip</Footer>
+            </Layout>
         </div>
     );
 }
