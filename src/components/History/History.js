2c37375c9c2cf650c48a8ab83b3f9f659f7eb4c0
Jason Dip <ContactJasonDip@gmail.com>
2020-05-22 00:44:28 -0700

    add icons to history page

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,22 +1,54 @@
 import React from "react";
 import { Timeline } from "antd";
+import {
+    CheckCircleOutlined,
+    PlayCircleOutlined,
+    CloseCircleOutlined,
+    ClockCircleOutlined,
+} from "@ant-design/icons";
 import styles from "./History.module.css";
 
 const History = (props) => {
     return (
         <div className={styles.container}>
-            <Timeline reverse mode="left">
-                <Timeline.Item label="MM-DD-YYYY" color="green">
-                    Title started (00:00)
+            <h3 style={{ textAlign: "center", paddingBottom: "20px" }}>
+                Timer History
+            </h3>
+            <Timeline className={styles.slowAnimation} reverse mode="left">
+                <Timeline.Item
+                    label="MM-DD-YYYY"
+                    color="green"
+                    dot={<PlayCircleOutlined />}
+                >
+                    Title (00:00) - Start
                 </Timeline.Item>
-                <Timeline.Item label="MM-DD-YYYY" color="blue">
-                    Title ended (00:00)
+                <Timeline.Item
+                    label="MM-DD-YYYY"
+                    color="blue"
+                    dot={<CheckCircleOutlined />}
+                >
+                    Title (00:00) - End
                 </Timeline.Item>
-                <Timeline.Item label="MM-DD-YYYY" color="green">
-                    Title started (00:00)
+                <Timeline.Item
+                    label="MM-DD-YYYY"
+                    color="green"
+                    dot={<PlayCircleOutlined />}
+                >
+                    Title (00:00) - Start
                 </Timeline.Item>
-                <Timeline.Item label="MM-DD-YYYY" color="red">
-                    Title cancelled (00:00)
+                <Timeline.Item
+                    label="MM-DD-YYYY"
+                    color="red"
+                    dot={<CloseCircleOutlined />}
+                >
+                    Title (00:00) - Cancel
+                </Timeline.Item>
+                <Timeline.Item
+                    label="MM-DD-YYYY"
+                    color="grey"
+                    dot={<ClockCircleOutlined />}
+                >
+                    Title (00:00) - Running
                 </Timeline.Item>
             </Timeline>
         </div>
